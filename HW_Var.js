33eebf5da9beadf1a8e49b034230b22420fe3148
Muthu <seleniumbootcamper>
2024-11-29 22:00:33 +0530

    Primitive_Data_Types_HW

---
+++ HW_Var.js
@@ -0,0 +1,11 @@
+let firstName = `First_Name`;
+let companyName = `Company_Name`;
+let mobileNumber = 5298847111;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log(`First Name: ${firstName}`);
+console.log(`Company Name: ${companyName}`);
+console.log('Mobile Number:'+mobileNumber);
+console.log('IsAutomation: '+isAutomation);
+console.log('hasPlaywright: '+typeof hasPlaywright);
